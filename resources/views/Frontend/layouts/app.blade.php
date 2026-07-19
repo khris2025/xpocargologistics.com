5a1aa8ac0bfc3b723be8eb2d7891e326eee68f69
no_name <no@email.com>
2026-07-19 03:17:35 +0100

    add smartsupp

--- resources/views/Frontend/layouts/app.blade.php
+++ resources/views/Frontend/layouts/app.blade.php
@@ -253,6 +253,20 @@
       <script src="{{ asset('assets/js/imagesloaded.pkgd.min.js') }}"></script>
       <script src="{{ asset('assets/js/isotope.min.js') }}"></script>
       <script src="{{ asset('assets/js/scripts.js') }}"></script>
+      <!-- Smartsupp Live Chat script -->
+      <script type="text/javascript">
+      var _smartsupp = _smartsupp || {};
+      _smartsupp.key = '7315118504a049dd71b48a1201d99cf3a76c005d';
+      window.smartsupp||(function(d) {
+      var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+      s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+      c.type='text/javascript';c.charset='utf-8';c.async=true;
+      c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+      })(document);
+      </script>
+      <noscript>Powered by <a href="https://www.smartsupp.com" target="_blank">Smartsupp</a></noscript>
+
+
      
 
       
